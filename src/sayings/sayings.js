61db8b658a3250f75777a1db055e0c0787754634
Matthew Lock <matthewwilliamlock@gmail.com>
2020-08-06 17:26:31 +0200

    Added responsive css for mobile surfing

--- src/sayings/sayings.js
+++ src/sayings/sayings.js
@@ -1,10 +1,11 @@
 lord_fay_sayings = [
-    "I'm lord Fay and imma be your daddy one day.",
+    "I'm Lord Fay and imma be your daddy one day.",
     "Mad Cute. Mad Smart. Mad Handsome.",
     "Imma break your arm.",
     "My kids are gonna be world class athletes.",
     "What you gonna do about it?",
-    "And baby lets go."
+    "And baby lets go.",
+    "And I'm a rock climber."
 ]
 
 module.exports = lord_fay_sayings
