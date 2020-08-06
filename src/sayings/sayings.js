24be5402221d984f5fdb283439abbfa3cdb7fd9b
Matthew Lock <matthewwilliamlock@gmail.com>
2020-08-06 11:32:15 +0200

    added saying to production

---
+++ src/sayings/sayings.js
@@ -0,0 +1,9 @@
+lord_fay_sayings = [
+
+    "Mad Cute. Mad Smart. Mad Handsome.",
+    "Imma break your arm.",
+    "My kids are gonna be world class athletes.",
+    "What you gonna do about it?",
+]
+
+module.exports = lord_fay_sayings
