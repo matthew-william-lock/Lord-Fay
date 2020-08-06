ffbb86e5dcd41f956eaa95fcac70e8f932f4036b
Matthew Lock <matthewwilliamlock@gmail.com>
2020-08-06 15:02:01 +0200

    Added more sayings

--- src/sayings/sayings.js
+++ src/sayings/sayings.js
@@ -1,8 +1,10 @@
 lord_fay_sayings = [
+    "I'm lord Fay and imma be your daddy one day.",
     "Mad Cute. Mad Smart. Mad Handsome.",
     "Imma break your arm.",
     "My kids are gonna be world class athletes.",
     "What you gonna do about it?",
+    "And baby lets go."
 ]
 
 module.exports = lord_fay_sayings
